35a16a6c3117408eccb61fe77e05d570b4984421
ariem <ariem@b740f020-a64b-42ef-93d7-576c64353ba2>
2009-10-06 11:58:15 +0000

    Fixed makefile for cygwin environment.

--- ruler-interpreter/gendeps.sh
+++ ruler-interpreter/gendeps.sh
@@ -4,7 +4,7 @@ echo "# Generated AG dependencies" > Makefile.depend
 
 for file in $@
 do
-  output=`cd src; $UUAGC --genfiledeps $file.ag | tr '\n' ' '`
+  output=`cd src; "$UUAGC" --genfiledeps $file.ag | tr '\n' ' '`
   echo "derived/$file.hs: src/$file.ag \$(addprefix src/, $output)" >> Makefile.depend
   echo "Makefile.depend: src/$file.ag \$(addprefix src/, $output)" >> Makefile.depend
 done
