e7c213f65fb066c754b92b2877d42d2e0abe6734
Arie Middelkoop <amiddelk@gmail.com>
2010-09-15 21:38:06 +0000

    Changed the ordering of rules. I'm now adding additional edges to the graph, such that a topsort of the graph actually gives the vertices in a total order.
    The reason is that the partial order imposed by
    attribute dependencies leaves freedom on how to
    order unrelated rules. For conventional AGs, this
    is not really important. However, I'm allowing
    multiple clauses per production. In this case,
    I want to exploit this freedom to move pattern
    matches to the front, and visits to children to
    the back of the visit order (while still
    respecting the attr dependencies).
    
    This simple idea turned out to be quite hard to
    implement. I even had to resort to two different
    graph libraries: the conventional Data.Graph
    library to compute stronlgy-connected components
    in topologically sorted order, and the FGL
    library to dynamically extend graphs.

--- ruler-core/src/DepAnalysis.hs
+++ ruler-core/src/DepAnalysis.hs
@@ -1,4 +1,4 @@
-module DepAnalysis {- (analyze) -} where
+module DepAnalysis (analyze) where
 
 import Common
 import Data.Graph
@@ -9,12 +9,21 @@ import qualified Data.Map as Map
 import Data.Map(Map)
 import qualified Data.Set as Set
 import Data.Set(Set)
-import Data.List(partition, nub)
+import qualified Data.IntSet as IntSet
+import Data.IntSet(IntSet)
+import Data.List(partition, nub,sortBy,groupBy)
+import Data.Array
+import Debug.Trace(trace)
 
-analyze :: [DepItem] -> [DepItem] -> Set DepItem -> [(Reason, DepItem, [DepItem])] -> [SCC DepItem]
-analyze oStartVisits oEndVisits avoidVisits deps = map compToItem comps
+import qualified Data.Graph.Inductive as G
+
+
+-- computes additional dependencies related to "late-as-possible" scheduling and avoidance of
+-- cyclic visits
+analyze :: [DepItem] -> [DepItem] -> [DepItem] -> Set DepItem -> [(Reason, DepItem, [DepItem])] -> [SCC DepItem]
+analyze oSemStarts oStartVisits oEndVisits avoidVisits deps = map compToItem comps
   where
-    comps = stronglyConnCompR deps'
+    comps = stronglyConnCompR $ total deps'
 
     compToItem (AcyclicSCC d) = AcyclicSCC (toItem d)
     compToItem (CyclicSCC ds) = CyclicSCC (map toItem ds)
@@ -82,3 +91,91 @@ analyze oStartVisits oEndVisits avoidVisits deps = map compToItem comps
                 else case mbD of
                        Nothing -> return ()
                        Just d' -> put (mbD, Map.insertWith (flip const) d d' mp)
+
+{-
+-- performs the actual ordering
+order :: (Ord k, Show k) => [k] -> [(a, k, [k])] -> [SCC (a, k, [k])]
+order _ [] = []
+order starts deps
+  = sccs'
+  where
+    vals = map convert' sccs
+    convert' (AcyclicSCC v) = [extract v]
+    convert' (CyclicSCC vs) = map extract vs
+  
+    (graph, lookupVal,lookupVert) = graphFromEdges deps
+    extract n = case lookupVal n of
+                  (_,a,_) -> a
+
+    verts  = vertices graph
+    reach  = map (\v -> concatMap (reachable graph) (graph ! v)) verts
+    pairs  = zip verts reach
+    sccs   = cluster $ sortBy cmp' pairs
+    sccs'  = map convert sccs
+    
+    convert (AcyclicSCC v) = AcyclicSCC (lookupVal v)
+    convert (CyclicSCC vs) = CyclicSCC (map lookupVal vs)
+
+    cluster []         = []
+    cluster ((v,r):xs) = c : cluster zs
+      where (ys,zs) = span (\(v', _) -> v' `elem` r) xs
+            c | null ys && not (v `elem` r) = AcyclicSCC v
+              | otherwise = CyclicSCC (v : map fst ys)
+    
+    cmp' (v1,r1) (v2,r2)
+      = let z = cmp (v1,r1) (v2,r2)
+        in trace (show (extract v1) ++ " ~ " ++ show (extract v2) ++ ": " ++ show z) z
+
+    cmp (v1,r1) (v2,r2)
+      | v1 == v2       = EQ
+      | v2Bv1 && v1Bv2 = EQ
+      | v1Bv2          = LT
+      | v2Bv1          = GT
+      | otherwise      = compare (extract v1) (extract v2)
+      where
+        v1Bv2 = v1 `elem` r2
+        v2Bv1 = v2 `elem` r1
+-}
+
+-- Returns the strongly-connected components in a total order
+total :: [(Reason, DepItem, [DepItem])] -> [(Reason, DepItem, [DepItem])]
+total deps = result
+  where
+    {-
+    toSCC [x] | not (IntSet.member x directCycs) = AcyclicSCC (toVal x)
+    toSCC xs = CyclicSCC (map toVal xs)
+    
+    components = G.scc gFinal
+    directCycs = IntSet.fromList (map G.node' (G.gsel (\c -> G.node' c `elem` G.suc' c) gFinal))
+    -}
+    
+    result = map (\(a,(r,bs)) -> (r,a,bs)) $ Map.assocs merged
+    merged = Map.unionWith merge
+               (Map.fromList [ (d, (ReasonOrder, [])) | (_, d) <- G.labNodes gFinal ])
+               (Map.fromListWith merge [ (toVal a, (r, [toVal b])) | (a,b,r) <- G.labEdges gFinal ])
+    
+    merge (_,as) (r,bs) = (r,nub (as ++ bs))
+    
+    toVal x = G.lab' (G.context gFinal x)
+    
+    items = nub ([ s | (_,s,_) <- deps ] ++ [ d | (_,_,ds) <- deps, d <- ds ])
+    (nodes, mp) = G.mkNodes G.new items
+    (Just edges) = G.mkEdges mp [ (s,d,r) | (r, s, ds) <- deps, d <- ds ]
+    
+    gInitial :: G.Gr DepItem Reason
+    gInitial = G.mkGraph nodes edges
+    gFinal = foldr extend gInitial [ (a,b) | a <- nodes, b <- nodes ]
+    
+    extend ((a,av),(b,bv)) g
+      | a == b = g
+      | not aBb && not bBa = case compare av bv of
+                               EQ -> g
+                               LT -> gBA
+                               GT -> gAB
+      | otherwise = g
+      where
+        gAB = G.insEdge (a,b,ReasonOrder) g
+        gBA = G.insEdge (b,a,ReasonOrder) g
+
+        aBb = a `elem` G.reachable b g
+        bBa = b `elem` G.reachable a g
