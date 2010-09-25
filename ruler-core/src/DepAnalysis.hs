8b1859a8c0f705774ac5a25c62e3dc46ba7e2c3a
ariem <ariem@b740f020-a64b-42ef-93d7-576c64353ba2>
2010-09-25 01:06:28 +0000

    Reimplemented rule-sorting algorithm. Old want didnt scale to example of Tamar. This one perhaps does. Does not work for cyclic graphs yet (nontermination).

--- ruler-core/src/DepAnalysis.hs
+++ ruler-core/src/DepAnalysis.hs
@@ -7,12 +7,15 @@ import Data.Maybe
 import Control.Monad.State.Strict
 import qualified Data.Map as Map
 import Data.Map(Map)
+import qualified Data.IntMap as IntMap
+import Data.IntMap(IntMap)
 import qualified Data.Set as Set
 import Data.Set(Set)
 import qualified Data.IntSet as IntSet
 import Data.IntSet(IntSet)
 import Data.List(partition, nub,sortBy,groupBy)
 import Data.Array
+import Data.Tree
 import Debug.Trace(trace)
 
 import qualified Data.Graph.Inductive as G
@@ -23,7 +26,7 @@ import qualified Data.Graph.Inductive as G
 analyze :: [DepItem] -> [DepItem] -> [DepItem] -> Set DepItem -> [(Reason, DepItem, [DepItem])] -> [SCC DepItem]
 analyze oSemStarts oStartVisits oEndVisits avoidVisits deps = map compToItem comps
   where
-    comps = stronglyConnCompR $ deps'
+    comps = stronglyConnCompR $ total $ deps'
 
     compToItem (AcyclicSCC d) = AcyclicSCC (toItem d)
     compToItem (CyclicSCC ds) = CyclicSCC (map toItem ds)
@@ -137,22 +140,16 @@ order starts deps
         v2Bv1 = v2 `elem` r1
 -}
 
+{-
 -- Returns the strongly-connected components in a total order
 total :: [(Reason, DepItem, [DepItem])] -> [(Reason, DepItem, [DepItem])]
 total deps = result
   where
-    {-
-    toSCC [x] | not (IntSet.member x directCycs) = AcyclicSCC (toVal x)
-    toSCC xs = CyclicSCC (map toVal xs)
-    
-    components = G.scc gFinal
-    directCycs = IntSet.fromList (map G.node' (G.gsel (\c -> G.node' c `elem` G.suc' c) gFinal))
-    -}
     
     result = map (\(a,(r,bs)) -> (r,a,bs)) $ Map.assocs merged
     merged = Map.unionWith merge
-               (Map.fromList [ (d, (ReasonOrder, [])) | (_, d) <- G.labNodes gFinal ])
-               (Map.fromListWith merge [ (toVal a, (r, [toVal b])) | (a,b,r) <- G.labEdges gFinal ])
+               (Map.fromListWith merge [ (toVal a, (ReasonOrder, [toVal b])) | (a,b) <- G.edges gFinal ])
+               (Map.fromList [ (a,(r,bs)) | (r,a,bs) <- deps ])
     
     merge (_,as) (r,bs) = (r,nub (as ++ bs))
     
@@ -164,18 +161,204 @@ total deps = result
     
     gInitial :: G.Gr DepItem Reason
     gInitial = G.mkGraph nodes edges
-    gFinal = foldr extend gInitial [ (a,b) | a <- nodes, b <- nodes ]
     
-    extend ((a,av),(b,bv)) g
-      | a == b = g
+    sinks = map G.node' $ G.gsel (null . G.suc') gInitial
+    backwards = G.dfs sinks gInitial
+    (_,gFinal) = foldl tighten (G.trc gInitial, gInitial) backwards
+    
+    tighten :: (G.Gr DepItem (), G.Gr DepItem Reason) -> G.Node -> (G.Gr DepItem (), G.Gr DepItem Reason)
+    tighten (g, g') n
+      = let ps    = G.suc gInitial n  -- successors in previous graph
+            ps'   = sortBy compare' ps
+            pairs = [ (a,b) | a <- ps', b <- ps', a /= b ]
+        in foldr extend (g, g') pairs
+    
+    extend (a,b) (g, g')
+      | not aBb && not bBa = case compare av bv of
+                               EQ -> (g, g')
+                               LT -> gBA
+                               GT -> gAB
+      | otherwise = (g, g')
+      where
+        (Just av) = G.lab g a
+        (Just bv) = G.lab g b
+      
+        gAB = (G.trc $ G.insEdge (a,b,()) g, G.insEdge (a,b,ReasonOrder) g')
+        gBA = (G.trc $ G.insEdge (b,a,()) g, G.insEdge (b,a,ReasonOrder) g')
+
+        aBb = a `elem` G.suc g b
+        bBa = b `elem` G.suc g a
+    
+    compare' a b
+      = let (Just av) = G.lab gInitial a
+            (Just bv) = G.lab gInitial b
+        in compare av bv
+-}
+
+-- Refined approach
+{-
+total :: [(Reason, DepItem, [DepItem])] -> [(Reason, DepItem, [DepItem])]
+total deps = result
+  where
+    result = map (\(a,(r,bs)) -> (r,a,bs)) $ Map.assocs merged
+    merged = Map.unionWith merge
+               (Map.fromListWith merge [ (toVal a, (ReasonOrder, [toVal b])) | (b,a) <- G.edges gFinal ])
+               (Map.fromList [ (a,(r,bs)) | (r,a,bs) <- deps ])
+    
+    merge (_,as) (r,bs) = (r,nub (as ++ bs))
+    toVal x = G.lab' (G.context gFinal x)
+    
+    items = nub ([ s | (_,s,_) <- deps ] ++ [ d | (_,_,ds) <- deps, d <- ds ])
+    (nodes, mp) = G.mkNodes G.new items
+    (Just edges) = G.mkEdges mp [ (s,d,r) | (r, s, ds) <- deps, d <- ds ]
+    
+    g0 :: G.Gr DepItem Reason
+    g0 = G.mkGraph nodes edges
+    g1 = removeCycles g0
+    
+    g2 :: G.Gr DepItem ()
+    g2 = G.trc (G.grev g1)
+    sinks = map G.node' $ G.gsel (null . G.suc') g2
+    backwards = G.dfs sinks g2
+    gFinal = g2 -- foldl tighten g2 backwards  -- note: graph is reversed compared to g0
+    
+    tighten :: G.Gr DepItem () -> G.Node -> G.Gr DepItem ()
+    tighten g n
+      = let ps    = G.suc g1 n  -- successors in older graph
+            pairs = [ (a,b) | a <- ps, b <- ps, a /= b ]
+        in foldr extend g pairs
+    
+    extend :: (G.Node, G.Node) -> G.Gr DepItem () -> G.Gr DepItem ()
+    extend (a,b) g
       | not aBb && not bBa = case compare av bv of
                                EQ -> g
                                LT -> gBA
                                GT -> gAB
       | otherwise = g
       where
-        gAB = G.insEdge (a,b,ReasonOrder) g
-        gBA = G.insEdge (b,a,ReasonOrder) g
+        (Just av) = G.lab g a
+        (Just bv) = G.lab g b
+        gAB = G.trc $ G.insEdge (b,a,()) g
+        gBA = G.trc $ G.insEdge (a,b,()) g
 
-        aBb = a `elem` G.reachable b g
-        bBa = b `elem` G.reachable a g
+        aBb = b `elem` G.suc g a
+        bBa = a `elem` G.suc g b
+    
+removeCycles :: G.Gr DepItem Reason -> G.Gr DepItem Reason
+removeCycles g0
+  = g2
+  where
+    sources = map G.node' $ G.gsel (null . G.suc') g0
+    forest = G.rdff sources g0
+    g1 = G.efilter (const False) g0
+    g2 = G.insEdges edges g1
+    edges = getEdges Nothing forest
+    
+    getEdges parent = concatMap (getEdges' parent)
+    getEdges' parent t
+      = let front = case parent of
+                      Nothing -> []
+                      Just p  -> [(rootLabel t, p, ReasonOrder)]
+        in front ++ getEdges (Just $ rootLabel t) (subForest t)
+-}
+
+-- totally new approach
+total :: [(Reason, DepItem, [DepItem])] -> [(Reason, DepItem, [DepItem])]
+total deps = result
+  where
+    result = map (\(a,(r,bs)) -> (r,a,bs)) $ Map.assocs merged
+    merged = Map.unionWith merge
+               (Map.fromListWith merge [ (a, (ReasonOrder, [b])) | (b,a) <- pairs ])
+               (Map.fromList [ (a,(r,bs)) | (r,a,bs) <- deps ])
+    
+    merge (_,as) (r,bs) = (r,nub (as ++ bs))
+    toVal x = case G.lab g0 x of
+                Nothing -> error ("has disapeared from graph: " ++ show x)
+                Just v  -> v
+    pairs = zip nodes3 (tail nodes3)
+    nodes3 = map toVal nodes2
+    
+    items = nub ([ s | (_,s,_) <- deps ] ++ [ d | (_,_,ds) <- deps, d <- ds ])
+    (nodes, mp) = G.mkNodes G.new items
+    (Just edges) = G.mkEdges mp [ (s,d,r) | (r, s, ds) <- deps, d <- ds ]
+    
+    g0 :: G.Gr DepItem Reason
+    g0 = G.mkGraph nodes edges     -- orig graph
+    g1 = G.trc $ g0 -- removeCycles g0   -- search graph
+    
+    nodes1 = sortBy compare' (G.nodes g0)  -- totally ordered
+    compare' a b
+      = let (Just av) = G.lab g0 a
+            (Just bv) = G.lab g0 b
+        in compare av bv
+    
+    dmap = list2DMap nodes1
+    nodes2 = toListDMap $ fixSolve (assocsDMap dmap) dmap
+    
+    fixSolve todo dmap
+      = case Map.maxViewWithKey todo of
+          Nothing -> dmap
+          Just ((a,v), todo1) ->
+            let preds = G.pre g1 v  -- a must be before the preds
+                troubles = filter (\b -> b < a) $ map (\v -> lookupDMapVal v dmap) preds
+            in if null troubles
+               then fixSolve todo1 dmap
+               else let b = minimum troubles
+                        succs = G.suc g1 v
+                    in case insertDMap b v (removeDMap a dmap) of
+                         Left dmap' ->  -- reindexed, rebuild todo
+                           let a' = lookupDMapVal v dmap'
+                               todo2 = Map.fromList [ (lookupDMapVal v dmap', v) | v <- Map.elems todo1 ]
+                               todo3 = Map.fromList [ (k,v) | v <- succs, let k = lookupDMapVal v dmap', k > a'] `Map.union` todo2
+                           in fixSolve todo3 dmap'
+                         Right dmap' ->
+                           let todo2 = Map.fromList [ (b,v) | v <- succs, let k = lookupDMapVal v dmap', b > a] `Map.union` todo1
+                           in fixSolve todo2 dmap'
+
+removeCycles :: G.Gr DepItem Reason -> G.Gr DepItem Reason
+removeCycles g0
+  = g2
+  where
+    sources = map G.node' $ G.gsel (null . G.suc') g0
+    forest = G.rdff sources g0
+    g1 = G.efilter (const False) g0
+    g2 = G.insEdges edges g1
+    edges = getEdges Nothing forest
+    
+    getEdges parent = concatMap (getEdges' parent)
+    getEdges' parent t
+      = let front = case parent of
+                      Nothing -> []
+                      Just p  -> [(rootLabel t, p, ReasonOrder)]
+        in front ++ getEdges (Just $ rootLabel t) (subForest t)
+
+newtype DMap = DMap (Map Double Int, IntMap Double)
+
+emptyDMap = DMap (Map.empty, IntMap.empty)
+
+lookupDMapKey k (DMap (m,_)) = Map.findWithDefault undefined k m
+lookupDMapVal v (DMap (_,m)) = IntMap.findWithDefault undefined v m
+
+assocsDMap (DMap (m,_)) = Map.deleteMin $ Map.deleteMax m  -- remove sentinels
+
+list2DMap xs = DMap (m, r)
+  where
+    pairs = zip [1.0, 2.0..] xs
+    m = Map.fromAscList ([(0.0,0)] ++ pairs ++ [(fromIntegral (length xs + 1),maxBound)])
+    r = IntMap.fromList [ (k,d) | (d,k) <- pairs ]
+
+-- insert just before b (potentially reassigns the elements in the map)
+insertDMap b v (DMap (m,r))
+  | c == a    = let mp1' = Map.deleteMin mp1
+                    mp2' = Map.deleteMax mp2
+                in Left $ list2DMap (Map.elems mp1' ++ [v, v'] ++ Map.elems mp2')
+  | otherwise = Right $ DMap (Map.insert c v m, IntMap.insert v c r)
+  where
+    (mp1,Just v',mp2) = Map.splitLookup b m
+    (a,_) = Map.findMax mp1
+    c = (a + b) / 2.0
+
+removeDMap a (DMap (m,r))
+  = DMap (Map.delete a m, r)
+
+toListDMap (DMap (m,_)) = tail $ init $ Map.elems m
